2a6a3608bd197d0d232e7fbcc1349052b5ebaf35
5mmx <53703932+5mmx@users.noreply.github.com>
2026-06-07 20:40:19 +0200

    .

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'https://mcityroleplay.github.io/MCityDistribution/distribution.json'
+exports.REMOTE_DISTRO_URL = 'https://mcityroleplay.github.io/Distribution/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
